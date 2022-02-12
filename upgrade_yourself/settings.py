575b5fca6ae3ea3af2a31813f0a31acec3ece9ad
Dejan <dejan24dr@gmail.com>
2022-02-12 11:52:32 +0100

    update settings.py base_dir part2

--- upgrade_yourself/settings.py
+++ upgrade_yourself/settings.py
@@ -17,7 +17,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = "/var/www/upgradeyoursef.tk/"
+BASE_DIR = "/var/www/upgradeyoursef.tk"
 
 
 # Quick-start development settings - unsuitable for production
